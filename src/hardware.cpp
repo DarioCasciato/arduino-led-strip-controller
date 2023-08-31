7a97524942cc70c43750e758676baba692db5945
Dario Casciato <dario.casciato@dormakaba.com>
2023-08-31 12:54:11 +0200

    changed C type casts to C++ type casts

--- src/hardware.cpp
+++ src/hardware.cpp
@@ -14,14 +14,14 @@ namespace Hardware
     uint8_t buttonState = 0;
     EdgeDetection button(&buttonState);
 
-    Potentiometer potFunction((uint8_t) Port::PotFunction,
+    Potentiometer potFunction(static_cast<uint8_t>(Port::PotFunction),
                               Direction::UP);
 
-    Potentiometer potBrightness((uint8_t) Port::PotBrightness,
+    Potentiometer potBrightness(static_cast<uint8_t>(Port::PotBrightness),
                                 Direction::UP);
 
     Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LIGHTS,
-                                                (uint8_t) Port::Strip,
+                                                static_cast<uint8_t>(Port::Strip),
                                                 NEO_RGB + NEO_KHZ800);
 
 
@@ -33,6 +33,6 @@ namespace Hardware
 
     void updateHardware()
     {
-        buttonState = GPIO::getPort((uint8_t) Port::Button);
+        buttonState = GPIO::getPort(static_cast<uint8_t>(Port::Button));
     }
 } // namespace Hardware
