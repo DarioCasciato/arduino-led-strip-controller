c41f705b2f844153127e59b13627606f5661b61c
Dario Casciato <93987804+DarioCasciato@users.noreply.github.com>
2023-09-01 19:50:18 +0200

    Added bootup sequence

--- src/main.cpp
+++ src/main.cpp
@@ -8,32 +8,53 @@
 #include "configurations.h"
 #include "Potentiometer.h"
 #include "state.h"
+#include "modes/strip_modes.h"
 
 void refreshData();
+void bootUpSequence();
 
 //------------------------------------------------------------------------------
 
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-  Hardware::init();
+    Hardware::init();
+
+    bootUpSequence();
 }
 
 void loop()
 {
-  for (;;)
-  {
-    refreshData();  // refresh port values
+    for (;;)
+    {
+        refreshData(); // refresh port values
 
-    State::stateDriver();
-  }
+        State::stateDriver();
+    }
 }
 
 //------------------------------------------------------------------------------
 
 void refreshData()
 {
-  Hardware::updateHardware();
-  EdgeDetection::updateEdges();
+    Hardware::updateHardware();
+    EdgeDetection::updateEdges();
+}
+
+void bootUpSequence()
+{
+    const uint16_t functionPotValue = Hardware::potFunction.get();
+    const uint8_t currentBrightness = map(Hardware::potBrightness.get(),
+                                          0, 1024, 1, 255);
+
+    Mode::white(functionPotValue);
+    FastLED.show();
+
+    for (uint8_t i = 0; i < currentBrightness; i++)
+    {
+        FastLED.setBrightness(i);
+        FastLED.show();
+        delay(8);
+    }
 }
