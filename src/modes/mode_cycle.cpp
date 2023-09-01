9eedd18f54353217c4b7770024e15721ce62dfe3
Dario Casciato <dario.casciato@dormakaba.com>
2023-09-01 10:53:56 +0200

    Also implemented colorpick and cycle mode

--- src/modes/mode_cycle.cpp
+++ src/modes/mode_cycle.cpp
@@ -4,8 +4,27 @@
 
 #include "strip_modes.h"
 #include "hardware.h"
+#include "Timer.h"
 
 void Mode::cycle(uint16_t functionValue)
 {
+    static Timer delayTime;
+    static uint8_t colorIndex = 0;
+    const CRGB outputColor = Color::getColorValue(colorIndex);
+    const uint8_t changeDelay = map(functionValue, 0, 1023, 0, 255);
 
+    for (uint8_t i = 0; i < NUM_LEDS; i++)
+        Hardware::leds[i] = outputColor;
+
+    FastLED.show();
+
+    // check if timer was started the first time
+    if(0 == delayTime.elapsedStart())
+        delayTime.start();
+
+    if(delayTime.elapsed(changeDelay))
+    {
+        colorIndex++;
+        delayTime.start();
+    }
 }
